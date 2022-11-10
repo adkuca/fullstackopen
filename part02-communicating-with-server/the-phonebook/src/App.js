9b62b371286357b75d9c86a05e8d4ee3beedb77d
adkuca <adkucauip@gmail.com>
2022-11-10 01:06:26 +0100

    Add exercise2_9 solution

--- part02-communicating-with-server/the-phonebook/src/App.js
+++ part02-communicating-with-server/the-phonebook/src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
 
   const handleNewNameChange = (event) => setNewName(event.target.value);
   const handleNewNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterChange = (event) => setNewFilter(event.target.value);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -15,13 +22,20 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    const newPerson = { name: newName, number: newNumber };
+    const newPerson = { id: persons.length + 1, name: newName, number: newNumber };
     setPersons((prev) => prev.concat(newPerson));
   };
+
+  const filteredPersons = persons.filter(({ name }) => new RegExp(newFilter, 'i').test(name));
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           <div>
@@ -37,8 +51,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(({ name, number }) => (
-          <li key={name}>
+        {filteredPersons.map(({ id, name, number }) => (
+          <li key={id}>
             {name} {number}
           </li>
         ))}
